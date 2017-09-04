ef9d07d175e7c13ed83ef63e35a86e167e70941a
Bailey Cosier <bailey.cosier@gmail.com>
2017-09-04 13:50:38 +0700

    Update cargo.toml for buildable Rumo demo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#[cfg(target_os = "android")]
+extern crate android_ffi;
+// extern crate kinito;
+
+use std::boxed::Box;
+
+#[no_mangle]
+#[inline(never)]
+#[allow(non_snake_case)]
+#[cfg(target_os = "android")]
+pub extern "C" fn rust_android_main(app: *mut ()) {
+    android_ffi::android_main2(app as *mut _, move |c, v| unsafe { main(c, v) });
 }
+
+#[cfg(target_os = "android")]
+fn main(_: isize, _: *const *const u8) {}
